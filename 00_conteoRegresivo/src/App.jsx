1d65ed690af46fb62e07ccf01c4165b5872a5f55
JSFranco96 <javierfranco0904@gmail.com>
2023-10-05 17:20:03 -0500

    feat: :tada: Beign project for linkedin learning course of react
    Adding a first project wich have a class based component

---
+++ 00_conteoRegresivo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ConteoRegresivo from './components/ConteoRegresivo'
+
+function App() {
+
+  return (
+    <main>
+      <ConteoRegresivo/>
+    </main>
+  )
+}
+
+export default App
